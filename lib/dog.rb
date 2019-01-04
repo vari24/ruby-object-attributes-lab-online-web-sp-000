c9677a2e9bcdbc44ce781b72a7855548ba523e0a
Sara Varisara <varisara.n@gmail.com>
2019-01-04 04:07:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog
 
   def name=(fido)
-    @this_dogs_name = fido
+    @this_dogs_name = Fido
   end
 
   def name
     @this_dogs_name
+  end
+
+  def breed=(beagle)
+    @this_dogs_breed = Beagle
+  end
+
+  def breed
+    @this_dogs_breed
   end
 end
